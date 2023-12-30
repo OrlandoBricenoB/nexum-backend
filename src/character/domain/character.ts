1eb8b6f2f9d87c39032a14c34961f22fa12be389
orlandobricenob <orlando.briceno.blanco@gmail.com>
2023-12-30 06:52:44 -0400

    feat: Has been added fields and getUniqueFields to domains

--- src/character/domain/character.ts
+++ src/character/domain/character.ts
@@ -1,11 +1,23 @@
+import { pick } from 'lodash'
+
 export class Character {
   public id: string
+  public account_id: string
   public name: string
-  public description: string
+  public kingdom: string
+  public division: string
 
-  constructor(id: string, name: string, description: string) {
+  public static fields: string[] = ['id', 'account_id', 'name', 'kingdom', 'division']
+
+  constructor(id: string, account_id: string, name: string, kingdom: string, division: string) {
     this.id = id
+    this.account_id = account_id
     this.name = name
-    this.description = description
+    this.kingdom = kingdom
+    this.division = division
+  }
+
+  public static getUniqueFields() {
+    return pick(Character.fields, ['id', 'account_id', 'name'])
   }
 }
