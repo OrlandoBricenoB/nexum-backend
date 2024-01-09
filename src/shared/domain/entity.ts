95a25603e12efe615e3e4a346bf639cd3975e243
Bennjamon <benjaminrivas500@gmail.com>
2024-01-09 00:09:20 -0300

    feature: Create entity class

---
+++ src/shared/domain/entity.ts
@@ -0,0 +1,11 @@
+import { pick } from 'lodash'
+
+export class Entity {
+  // eslint-disable-next-line @typescript-eslint/no-explicit-any
+  static create<T extends Entity>(this: new (...args: any[]) => T, data: Partial<T>): Partial<T> {
+    const empty = new this()
+    const keys = Object.keys(empty)
+
+    return pick(data, keys)
+  }
+}
