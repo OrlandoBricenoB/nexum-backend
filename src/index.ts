06f67135c9abf75fefa961cb5c3307ca3c869e10
orlandobricenob <orlando.briceno.blanco@gmail.com>
2024-02-19 13:50:47 -0400

    fix: Has been removed tcpClient from index

--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,9 @@
 import { ExpressServer } from './server/services/expressServer'
 import { registerRoutes } from './server/routes'
 import { config } from 'dotenv'
-import { TcpClient } from './tcp-client/services/tcpClient'
 config()
 
 const server = new ExpressServer()
-const tcpClient = new TcpClient()
 const app = server.getApp()
 
 registerRoutes(app)
@@ -13,8 +11,3 @@ registerRoutes(app)
 // * HTTP Server
 const port = process.env.PORT || 3000
 server.listen(Number(port))
-
-// * TCP Client
-tcpClient.send({
-  name: 'jo'
-})
