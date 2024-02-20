984edd4dfc3a1316589ae2e32bcebfb28952299a
Bennjamon <benjaminrivas500@gmail.com>
2024-02-19 22:50:12 -0300

    feature: Implement get characters by account tcp route

--- src/tcp-client/controllers/tcpRequests.ts
+++ src/tcp-client/controllers/tcpRequests.ts
@@ -1,10 +1,15 @@
 import { Request, Response } from 'express'
 import { ControllerBase } from '../../shared/domain/controllerBase'
 import { TcpClient } from '../services/tcpClient'
+import { CharacterService } from '../../character/services/characterService'
 
 export default class TcpRequestsController extends ControllerBase {
+  private characterService: CharacterService
+
   constructor() {
     super()
+
+    this.characterService = new CharacterService()
   }
 
   public async getAllData(req: Request, res: Response): Promise<void> {
@@ -12,6 +17,21 @@ export default class TcpRequestsController extends ControllerBase {
     const tcpClient = new TcpClient()
     tcpClient.send({
       data: 'all'
+    })
+
+    res.send('Done.')
+  }
+
+  public async getCharactersByAccount(req: Request, res: Response): Promise<void> {
+    const tcpClient = new TcpClient()
+
+    const { account_id } = req.body as { account_id: string }
+
+    const characters = await this.characterService.getAllAccountCharacters(account_id)
+
+    tcpClient.send({
+      action: 'charactersByAccount',
+      data: characters
     })
 
     res.send('Done.')
--- src/tcp-client/routes/tcpRequestRoutes.ts
+++ src/tcp-client/routes/tcpRequestRoutes.ts
@@ -5,5 +5,6 @@ const tcpRequests = new TcpRequestsController()
 const router = Router()
 
 router.get('/', tcpRequests.getAllData)
+router.post('/characters/getByAccount', tcpRequests.getAllCharacters)
 
 export const TcpRequestRouter = router
